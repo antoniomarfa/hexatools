607ff57d977d95238175cfc4f91420b5ce746fd5
antojama <a.martinez@westorage.cl>
2025-05-08 12:12:46 -0400

    v1.0.3

--- api/middlewares/recover.go
+++ api/middlewares/recover.go
@@ -8,28 +8,6 @@ import (
 )
 
 // Recover is a middleware function to defer and return an error response in case of panic during the handler execution
-/*
-func Recover(next http.HandlerFunc) http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		defer func() {
-			var err error
-			if rec := recover(); rec != nil {
-				switch t := rec.(type) {
-				case error:
-					err = t
-				case string:
-					err = fmt.Errorf(t)
-				default:
-					err = fmt.Errorf("unknown error ocurred")
-				}
-				utils.ResponseError(w, r, nil, err)
-			}
-		}()
-		next(w, r)
-	})
-}
-*/
-
 func Recover() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		defer func() {
