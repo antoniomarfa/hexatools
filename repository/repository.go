4e397c0b73837563a36f2d34e981dba7d0091bd3
antojama <a.martinez@westorage.cl>
2025-05-08 14:10:07 -0400

    v1.0.5

--- repository/repository.go
+++ repository/repository.go
@@ -8,7 +8,6 @@ type Repository interface {
 	Get(ctx context.Context, filter map[string]interface{}, skip, take *int) ([]interface{}, error)
 	GetInf(ctx context.Context, filter map[string]interface{}, skip, take *int) ([]interface{}, error)
 	GetByID(ctx context.Context, ID string) (interface{}, error)
-	GetUpdateByID(ctx context.Context, ID string) (interface{}, error)
 	Update(ctx context.Context, ID string, entity interface{}) error
 	Delete(ctx context.Context, ID string, filter map[string]interface{}) error
 }
