ffbfaaa0c916e4a94c6c3d76d4136bad006a846c
amartinez <soandasystem@gmail.com>
2026-08-26 10:12:27 -0400

    v1.0.10 fix/modificacion cors

--- infrastructure/middleware/cors.go
+++ infrastructure/middleware/cors.go
@@ -8,9 +8,26 @@ import (
 )
 
 // CORSMiddleware configura CORS en la API
+/*
 func CORSMiddleware() gin.HandlerFunc {
 	return cors.New(cors.Config{
 		AllowOrigins:     []string{"*"}, // En producción, restringe los dominios permitidos
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "Accept", "x-tenant-schema"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	})
+}
+*/
+
+// CORSMiddleware configura CORS en la API
+func CORSMiddleware() gin.HandlerFunc {
+	return cors.New(cors.Config{
+		// Permite cualquier origen de forma dinámica respondiendo con el origen que hace la solicitud (compatible con AllowCredentials)
+		AllowOriginFunc: func(origin string) bool {
+			return true
+		},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "Accept", "x-tenant-schema"},
 		ExposeHeaders:    []string{"Content-Length"},
